1191b24c89fc09380a7fccebf9f03e6e714a2018
pillaca27 <cristal_27.2011@hotmail.com>
2021-06-22 16:33:20 -0500

    fgdfgfgf

---
+++ DSWI-TiendaJulio/Controllers/ConsultasController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSWI_TiendaJulio.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        
+    }
+}
